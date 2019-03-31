894a50f85ee3207b1b48e69d1e99c09cd25e2079
wenying <2515957145@qq.com>
2019-03-31 19:41:21 +0800

    数组中只出现一次的数字

---
+++ 剑指Offer/31.数组中只出现一次的数字.py
@@ -0,0 +1,19 @@
+"""
+题目描述
+一个整型数组里除了两个数字之外，其他的数字都出现了两次。
+请写程序找出这两个只出现一次的数字。
+"""
+
+
+class Solution:
+    # 返回[a,b] 其中ab是出现一次的两个数字
+    def FindNumsAppearOnce(self, array):
+        # write code here
+        arr = []
+        for i in array:
+            if len(arr) == 2:
+                return arr
+            else:
+                if array.count(i) == 1:
+                    arr.append(i)
+        return arr
