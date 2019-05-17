cd910d31a71859eda522bef5174346714c561419
CodingBuye <2515957145@qq.com>
2019-05-17 11:32:25 +0800

    动态规划：剪绳子

---
+++ 剑指Offer/14.剪绳子.py
@@ -0,0 +1,30 @@
+"""
+题目描述：
+给你一根长度为n的绳子，请把绳子剪成m段(m、n都是整数，并且n>1,m>1).
+每段绳子的长度记为k[0],k[1],...,k[m],
+求k[0]×k[1]×...×k[m]乘积最大是多少？
+"""
+class Solution:
+
+    def max_product_after_cutting_solution1(self, length):
+        if length < 2:
+            return 0
+        if length == 2:
+            return 1
+        if length == 3:
+            return 2
+        products = [0, 1, 2, 3]
+
+        for i in range(4, length+1):
+            max_val = 0
+            for j in range(1, i//2 + 1):
+                product = products[j] * products[i-j]
+                if max_val < product:
+                    max_val = product
+            products.append(max_val)
+        return products[length]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.max_product_after_cutting_solution1(8))
