1c76605799592791f371b81b4c4b32a4c3b48307
wenying <2515957145@qq.com>
2019-06-03 13:36:19 +0800

    Merge branch 'master' of E:\PycharmFolder\PythonForNowcoder with conflicts.

--- 剑指Offer/14.剪绳子.py
+++ 剑指Offer/14.剪绳子.py
@@ -1,48 +1,8 @@
 """
 题目描述：
-给你一根长度为n的绳子，请把绳子剪成m段(m、n都是整数，并且n>1,m>1).
-每段绳子的长度记为k[0],k[1],...,k[m],
-求k[0]×k[1]×...×k[m]乘积最大是多少？
+给你一根长度为n的绳子，请把绳子剪成m段(m、n都是整数，n>1并且m>1),
+每段绳子的长度记为k[0],k[1],k[2],......,k[m]。
+请问k[0]×k[1]×......×k[m]可能的最大乘积是多少？例如，当绳子的长度是8时，
+我们把它剪成长度分别为2,3,3的三段，此时得到的最大乘积是18
 """
 
-
-class Solution:
-
-    def max_product_after_cutting_solution1(self, length):
-        if length < 2:
-            return 0
-        if length == 2:
-            return 1
-        if length == 3:
-            return 2
-        products = [0, 1, 2, 3]
-
-        for i in range(4, length+1):
-            max_val = 0
-            for j in range(1, i//2 + 1):
-                product = products[j] * products[i-j]
-                if max_val < product:
-                    max_val = product
-            products.append(max_val)
-        return products[length]
-
-    # 贪婪算法
-    def max_product_after_cutting_solution2(self, length):
-        if length < 2:
-            return 0
-        if length == 2:
-            return 1
-        if length == 3:
-            return 2
-
-        time_of_3 = length // 3
-        if length - time_of_3*3 == 1:
-            time_of_3 -= 1
-        time_of_2 = (length-time_of_3*3) // 2
-        return pow(3, time_of_3) * pow(2, time_of_2)
-
-
-if __name__ == '__main__':
-    s = Solution()
-    print(s.max_product_after_cutting_solution1(5))
-    print(s.max_product_after_cutting_solution2(8))
--- 剑指Offer/15.二进制中1的个数.py
+++ 剑指Offer/15.二进制中1的个数.py
@@ -1,20 +1,4 @@
 """
 题目描述
-输入一个整数，输出该数二进制表示中1的个数。
-其中负数用补码表示。
+输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
 """
-
-
-class Solution:
-    def NumberOf1(self, n):
-        if n < 0:
-            n = n & 0xffffffff
-        k = 0
-        while n:
-            k += 1
-            n = (n - 1) & n
-        return k
-
-
-if __name__ == '__main__':
-    print(Solution().NumberOf1(-1))
